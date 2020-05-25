f93091a0e96924efd403aa363bf08eb801d7fdce
Fabio Hedayioglu <fd85@kent.ac.uk>
2020-05-25 18:56:28 +0100

    -Updated windows binaries and install scripts

--- installers/anaconda/elongation_simulators/bld.bat
+++ installers/anaconda/elongation_simulators/bld.bat
@@ -1,10 +1,21 @@
+# load vstudio environment
+#set VSCMD_DEBUG=1
+#set VS_YEAR=2019
+#"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\vsdevcmd"
+#if errorlevel 1 exit 1
+
+#build all simulators
+#msbuild "%SRC_DIR%"\elongation_simualtors.sln -p:Configuration=Release
+#if errorlevel 1 exit 1
+
+
 cd installers\bin
-if errorlevel 1 exit 1
+#if errorlevel 1 exit 1
 
 copy ribosomesimulator.pyd "%PREFIX%"
 if errorlevel 1 exit 1
-copy vcruntime140_1.dll "%PREFIX%"
+copy translation.pyd "%PREFIX%"
 if errorlevel 1 exit 1
-copy vcruntime140.dll "%PREFIX%"
-if errorlevel 1 exit 1
+#copy vcruntime140.dll "%PREFIX%"
+#if errorlevel 1 exit 1
 
