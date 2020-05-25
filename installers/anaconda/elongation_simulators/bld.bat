456ef9858d66787a4e0fae5cd4130620e946e558
Fabio Hedayioglu <fd85@kent.ac.uk>
2020-05-25 23:51:39 +0100

    -copying dll files.

--- installers/anaconda/elongation_simulators/bld.bat
+++ installers/anaconda/elongation_simulators/bld.bat
@@ -12,10 +12,16 @@ rem if errorlevel 1 exit 1
 cd installers\bin
 rem if errorlevel 1 exit 1
 
-copy ribosomesimulator.pyd "%PREFIX%"
+copy vcruntime140.dll "%PREFIX%"\
 if errorlevel 1 exit 1
-copy translation.pyd "%PREFIX%"
+copy ribosomesimulator.pyd "%PREFIX%"\
 if errorlevel 1 exit 1
-rem copy vcruntime140.dll "%PREFIX%"
-rem if errorlevel 1 exit 1
+copy translation.pyd "%PREFIX%"\
+if errorlevel 1 exit 1
+mkdir "%PREFIX%"\concentrations
+if errorlevel 1 exit 1
+copy "%SRC_DIR%"\concentrations\__init__.py "%PREFIX%"\concentrations\
+if errorlevel 1 exit 1
+copy "%SRC_DIR%"\concentrations\Saccharomyces_cerevisiae.csv "%PREFIX%"\concentrations\
+if errorlevel 1 exit 1
 
