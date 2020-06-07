7c11c23fe6a64b545f69868fcfa30fdc81fa82a2
Fabio Hedayioglu <fd85@kent.ac.uk>
2020-06-07 20:12:12 +0100

    - added elongation test to check minimum space between elongating ribosomes

--- Unit-Tests/translatortester_test.cpp
+++ Unit-Tests/translatortester_test.cpp
@@ -1,10 +1,12 @@
 #include <gtest/gtest.h>
 #include <stdlib.h>
+#include <numeric>
 #include "../translation.h"
 
-TEST(TranslatorTester, simulateAAAx100xlowInitxHighTerm) {
+TEST(TranslatorTester, simulateAAAx100xlowInitxHighTerm)
+{
   Simulations::Translation ts;
-  char const* home_dir;
+  char const *home_dir;
   home_dir = getenv("HOME");
   std::string home_path(home_dir);
   ts.loadConcentrations(home_path +
@@ -26,7 +28,8 @@ TEST(TranslatorTester, simulateAAAx100xlowInitxHighTerm) {
       ts.getEnlogationDuration();
   ts.getInitiationElongationTermination();
   double total = 0;
-  for (auto dur : ts.elongations_durations) total += dur;
+  for (auto dur : ts.elongations_durations)
+    total += dur;
   double average = total / ts.elongations_durations.size();
   ASSERT_LE(average / 100, 0.052 * 1.1);
   ASSERT_GE(average / 100, 0.052 * 0.9);
@@ -40,6 +43,32 @@ TEST(TranslatorTester, simulateAAAx100xlowInitxHighTerm) {
   ASSERT_LE(average, 0.052 * 1.1);
   ASSERT_GE(average, 0.052 * 0.9);
   std::cerr << "\naverage codon_usage: " << average << "\n";
+}
+
+TEST(TranslatorTester, checkSpaceBetweenRibosomes)
+{
+  Simulations::Translation ts;
+  char const *home_dir;
+  home_dir = getenv("HOME");
+  std::string home_path(home_dir);
+  ts.loadConcentrations(home_path +
+                        "/Projects/RSim/data_2011/concentrations.csv");
+  ts.loadMRNA(home_path +
+              "/Projects/translation_results/initiation_frequency_tests/"
+              "20-09-2017/genes/AAA.txt");
+  ts.setInitiationRate(100);
+  ts.setTerminationRate(1000);
+  ts.setFinishedRibosomes(1000);
+
+  ts.setPrepopulate(true);
+  ts.run();
+  for (auto ribosomes_positions : ts.ribosome_positions_history)
+  {
+    for (std::size_t i = 1; i < ribosomes_positions.size(); i++)
+    {
+      ASSERT_GE(ribosomes_positions[i] - ribosomes_positions[i - 1], 10) << "\nOverlapping ribosomomes detected at positions: " << i - 1 << " and " << i <<", codons: "<<ribosomes_positions[i]<<" "<<ribosomes_positions[i-1]<< "\n";
+    }
+  }
 }
 
 TEST(TranslatorTester, Initialize) { EXPECT_EQ(1, 1); }
