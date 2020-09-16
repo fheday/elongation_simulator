a386af6c1cf54ded51171c6f213968d5987b4b2a
Fabio Hedayioglu <fd85@kent.ac.uk>
2020-09-16 14:21:13 +0100

    - added method to remove vector of ribosome positions. - added method to calculate ribosome collisions. - added method to save updated data to json file. - Exported above method to Python.

--- elongation_simulation_processor.cpp
+++ elongation_simulation_processor.cpp
@@ -9,17 +9,22 @@
 #include <pybind11/stl.h>
 namespace py = pybind11;
 
-void init_simulation_processor(py::module &mod) 
- {
+void init_simulation_processor(py::module &mod) {
   py::class_<Simulations::SimulationProcessor>(mod, "SimulationProcessor")
       .def(py::init<std::string>()) // constructor
       .def("getClock", &Simulations::SimulationProcessor::getClock)
-      .def("getRibosomes", &Simulations::SimulationProcessor::getRibosomes);
+      .def("getRibosomes", &Simulations::SimulationProcessor::getRibosomes)
+      .def("removeRibosomePositions", &Simulations::SimulationProcessor::removeRibosomePositions)
+      .def("calculateRibosomeCollisions", &Simulations::SimulationProcessor::calculateRibosomeCollisions)
+      .def("getCollidingRibosomes", &Simulations::SimulationProcessor::getCollidingRibosomes)
+      .def("getStalledRibosomes", &Simulations::SimulationProcessor::getStalledRibosomes)
+      .def("save", &Simulations::SimulationProcessor::save);
 }
 #endif
 
 
 Simulations::SimulationProcessor::SimulationProcessor(std::string file_name) {
+    configuration_file_name = file_name;
     std::ifstream config_doc(file_name, std::ifstream::binary);
     Json::Value root; // the json document.
     config_doc >> root;
@@ -59,3 +64,74 @@ std::vector<std::vector<int>>& Simulations::SimulationProcessor::getRibosomes() 
     return elongating_ribosomes;
 }
 
+void Simulations::SimulationProcessor::removeRibosomePositions() {
+    elongating_ribosomes.clear();
+}
+
+void Simulations::SimulationProcessor::calculateRibosomeCollisions() {
+    for (auto ribosomes_positions:elongating_ribosomes) {
+        std::vector<int> collision_entry;
+        std::vector<int> stall_entry;
+        for (std::size_t i = 0; i < ribosomes_positions.size() - 1; i++) {
+            if (ribosomes_positions[i + 1] - ribosomes_positions[i] == 10) {
+                // colliding ribosome: collision with the next ribosome detected.
+                collision_entry.emplace_back(ribosomes_positions[i]);
+            } else if (!collision_entry.empty() && ribosomes_positions[i] - collision_entry.back() == 10) {
+                //stalled ribosome: no collision with next ribosome,
+                //but collision with previous ribosome detected.
+                stall_entry.emplace_back(ribosomes_positions[i]);
+            }
+        }
+        // check last entry. it can only stall.
+        if (!collision_entry.empty() && ribosomes_positions.back() - collision_entry.back() == 10) {
+            //stalled ribosome: no collision with next ribosome,
+            //but collision with previous ribosome detected.
+            stall_entry.emplace_back(ribosomes_positions.back());
+        }
+        colliding_ribosomes.emplace_back(collision_entry);
+        stalled_ribosomes.emplace_back(stall_entry);
+    }
+}
+
+std::vector<std::vector<int>>& Simulations::SimulationProcessor::getCollidingRibosomes() {
+    return colliding_ribosomes;
+}
+
+std::vector<std::vector<int>>& Simulations::SimulationProcessor::getStalledRibosomes() {
+    return stalled_ribosomes;
+}
+
+void Simulations::SimulationProcessor::save() {
+    Json::Value newjson;
+    newjson["fasta_file"] = fasta_file;
+    newjson["initiation_rate"] = initiation_rate;
+    newjson["termination_rate"] = termination_rate;
+    
+    auto generate_json_vector_of_vector = [&](auto data_vector) {
+        Json::Value json_value;
+        for (auto entry:data_vector){
+            Json::Value entry_vector;
+            for (auto element:entry) entry_vector.append(element);
+            json_value.append(entry_vector);
+        }
+        return json_value;
+    };
+
+
+    for (auto time:clock) newjson["clock"].append(time);
+
+    newjson["elongating_ribosomes"] = generate_json_vector_of_vector(elongating_ribosomes);
+    newjson["colliding_ribosomes"] = generate_json_vector_of_vector(colliding_ribosomes);
+    newjson["stalling_ribosomes"] = generate_json_vector_of_vector(stalled_ribosomes);
+
+    // std::ifstream file_stream(configuration_file_name, std::ifstream::binary);
+    // write in a nice readible way
+    Json::StreamWriterBuilder builder;
+    builder["commentStyle"] = "None"; // no comments.
+    builder["indentation"] = "   ";   // four spaces identation
+    std::unique_ptr<Json::StreamWriter> writer(builder.newStreamWriter());
+    std::ofstream config_doc_writer(configuration_file_name,
+                                        std::ifstream::binary);
+    writer->write(newjson, &config_doc_writer);
+    return;
+}
--- elongation_simulation_processor.h
+++ elongation_simulation_processor.h
@@ -11,12 +11,21 @@ namespace Simulations {
         SimulationProcessor(std::string);
         std::vector<float>& getClock();
         std::vector<std::vector<int>>& getRibosomes();
+        void removeRibosomePositions();
+        void calculateRibosomeCollisions();
+        std::vector<std::vector<int>>& getCollidingRibosomes();
+        std::vector<std::vector<int>>& getStalledRibosomes();
+        void save();
+
         private:
+        std::string configuration_file_name;
         std::string fasta_file;
         float initiation_rate;
         float termination_rate;
         std::vector<float> clock;
         std::vector<std::vector<int>> elongating_ribosomes;
+        std::vector<std::vector<int>> colliding_ribosomes;
+        std::vector<std::vector<int>> stalled_ribosomes;
     };
 }
 #endif
