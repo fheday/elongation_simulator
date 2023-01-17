6f77a1782556a7b46b78d0131a734d77a65feaac
Fabio Hedayioglu <fheday@gmail.com>
2023-01-17 15:51:42 +0000

    - fixed tests - some tests now use loadConcentrationsFromString

--- Unit-Tests/mrna_reader_test.cpp
+++ Unit-Tests/mrna_reader_test.cpp
@@ -5,7 +5,7 @@ TEST(mRNAReaderTester, open_simple_fasta_file)
 {
     //Test to read file with only one DNA sequence without header.
     mRNA_utils::mRNAReader reader;
-    reader.loadmRNAFile("data/mRNAs/MaxCFLuc.txt");
+    reader.loadmRNAFile("/mnt/Projects/Gillespie_Cpp/data/mRNAs/MaxCFLuc.txt");
     ASSERT_EQ(reader.getCodon(23), "GAA");
     ASSERT_EQ(reader.getCodon(21), "GCU"); // the reader converts T's to U's automatically.
 }
@@ -13,13 +13,13 @@ TEST(mRNAReaderTester, open_simple_fasta_file)
 TEST(mRNAReaderTester, open_fasta_file_one_gene_with_header) {
     //Test to read file with only one DNA sequence with header.
     mRNA_utils::mRNAReader reader;
-    reader.loadmRNAFile("data/mRNAs/RTN3.txt");
+    reader.loadmRNAFile("/mnt/Projects/Gillespie_Cpp/data/mRNAs/RTN3.txt");
     ASSERT_EQ(reader.getCodon(23), "GCG");
     ASSERT_EQ(reader.getCodon(22), "UCC"); // the reader converts T's to U's automatically.
     ASSERT_EQ(reader.getCodon(21), "CCG");
     //check if this works when a file has more than one gene.
     //In this case, we should get the first one only.
-    reader.loadmRNAFile("data/mRNAs/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa");
+    reader.loadmRNAFile("/mnt/Projects/Gillespie_Cpp/data/mRNAs/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa");
     ASSERT_EQ(reader.getCodon(0), "AUG");
     ASSERT_EQ(reader.getCodon(1), "UUC");
     ASSERT_EQ(reader.getCodon(2), "AGC");
@@ -29,7 +29,7 @@ TEST(mRNAReaderTester, open_fasta_file_one_gene_with_header) {
 TEST(mRNAReaderTester, open_fasta_file_many_genes_with_header) {
     //Test to read file with only one DNA sequence with header.
     mRNA_utils::mRNAReader reader;
-    const std::string fasta_file = "data/mRNAs/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa";
+    const std::string fasta_file = "/mnt/Projects/Gillespie_Cpp/data/mRNAs/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa";
     reader.loadGene(fasta_file, "YHR055C");
     ASSERT_EQ(reader.getCodon(0), "AUG");
     ASSERT_EQ(reader.getCodon(1), "UUC");
--- Unit-Tests/translatortester_test.cpp
+++ Unit-Tests/translatortester_test.cpp
@@ -1,7 +1,77 @@
 #include <gtest/gtest.h>
 #include <stdlib.h>
 #include <numeric>
+#include <string>
 #include "../translation.h"
+
+std::string concentrationsString = R"(codon,three.letter,WCcognate.conc,wobblecognate.conc,nearcognate.conc
+AAA,Lys,4.90774907749077E-06,0,1.40221402214022E-06
+AAC,Asn,7.01107011070111E-06,0,1.05166051660517E-05
+AAG,Lys,9.81549815498155E-06,0,6.309963099631E-06
+AAU,Asn,7.01107011070111E-06,0,1.19188191881919E-05
+ACA,Thr ,2.80442804428044E-06,0,1.75276752767528E-05
+ACC,Thr ,7.71217712177122E-06,0,1.12177121771218E-05
+ACG,Thr ,7.01107011070111E-07,0,2.80442804428044E-06
+ACU,Thr ,7.71217712177122E-06,0,1.05166051660517E-05
+AGA,Arg,7.71217712177122E-06,0,0
+AGC,Ser,2.10332103321033E-06,0,1.05166051660517E-05
+AGG,Arg,1.40221402214022E-06,0,1.19188191881919E-05
+AGU,Ser,2.10332103321033E-06,0,1.05166051660517E-05
+AUA,Ile,1.40221402214022E-06,0,1.40221402214022E-05
+AUC,Ile,9.11439114391144E-06,0,8.41328413284133E-06
+AUG,Met,3.50553505535055E-06,0,8.41328413284133E-06
+AUU,Ile,9.11439114391144E-06,0,8.41328413284133E-06
+CAA,Gln,5.60885608856089E-06,0,7.01107011070111E-06
+CAC,His,4.90774907749077E-06,0,1.82287822878229E-05
+CAG,Gln,7.01107011070111E-07,0,1.54243542435424E-05
+CAU,His,4.90774907749077E-06,0,1.2619926199262E-05
+CCA,Pro,7.01107011070111E-06,0,2.38376383763838E-05
+CCC,Pro,1.40221402214022E-06,0,2.24354243542435E-05
+CCG,Pro,7.01107011070111E-06,0,2.80442804428044E-06
+CCU,Pro,1.40221402214022E-06,0,2.24354243542435E-05
+CGA,Arg,4.20664206642066E-06,0,7.71217712177122E-06
+CGC,Arg,4.20664206642066E-06,0,4.90774907749077E-06
+CGG,Arg,7.01107011070111E-07,0,4.90774907749077E-06
+CGU,Arg,4.20664206642066E-06,0,4.90774907749077E-06
+CUA,Leu,2.10332103321033E-06,0,1.54243542435424E-05
+CUC,Leu,7.01107011070111E-07,0,1.82287822878229E-05
+CUG,Leu,2.10332103321033E-06,0,1.05166051660517E-05
+CUU,Leu,2.10332103321033E-06,0,1.68265682656827E-05
+GAA,Glu,9.81549815498155E-06,0,6.309963099631E-06
+GAC,Asp,1.12177121771218E-05,0,2.24354243542435E-05
+GAG,Glu,1.40221402214022E-06,0,1.96309963099631E-05
+GAU,Asp,1.12177121771218E-05,0,2.38376383763838E-05
+GCA,Ala,3.50553505535055E-06,0,2.80442804428044E-05
+GCC,Ala,7.71217712177122E-06,0,1.40221402214022E-05
+GCG,Ala,3.50553505535055E-06,0,1.40221402214022E-06
+GCU,Ala,7.71217712177122E-06,0,1.40221402214022E-05
+GGA,Gly,2.10332103321033E-06,0,7.71217712177122E-06
+GGC,Gly,1.12177121771218E-05,0,7.01107011070111E-06
+GGG,Gly,1.40221402214022E-06,0,7.01107011070111E-06
+GGU,Gly,1.12177121771218E-05,0,7.01107011070111E-06
+GUA,Val,1.40221402214022E-06,0,2.5239852398524E-05
+GUC,Val,9.81549815498155E-06,0,1.61254612546125E-05
+GUG,Val,1.40221402214022E-06,0,1.05166051660517E-05
+GUU,Val,9.81549815498155E-06,0,1.61254612546125E-05
+UAA,Stop,1.14E-06,0,1.19188191881919E-05
+UAC,Tyr,5.60885608856089E-06,0,1.75276752767528E-05
+UAG,Stop,1.14E-06,0,2.24354243542435E-05
+UAU,Tyr,5.60885608856089E-06,0,1.8929889298893E-05
+UCA,Ser,2.10332103321033E-06,0,3.01476014760148E-05
+UCC,Ser,7.71217712177122E-06,0,1.12177121771218E-05
+UCG,Ser,7.01107011070111E-07,0,1.75276752767528E-05
+UCU,Ser,7.71217712177122E-06,0,1.12177121771218E-05
+UGA,Stop,1.14E-06,0,1.19188191881919E-05
+UGC,Cys,2.80442804428044E-06,0,6.309963099631E-06
+UGG,Trp,4.20664206642066E-06,0,2.54221402214022E-06
+UGU,Cys,2.80442804428044E-06,0,6.309963099631E-06
+UUA,Leu,4.90774907749077E-06,0,1.2619926199262E-05
+UUC,Phe,7.01107011070111E-06,0,1.68265682656827E-05
+UUG,Leu,7.01107011070111E-06,0,1.05166051660517E-05
+UUU,Phe,7.01107011070111E-06,0,1.68265682656827E-05
+)";
+
+std::string mRNA_aaa = "AUGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAUGA";
 
 TEST(TranslatorTester, simulateAAAx100xlowInitxHighTerm)
 {
@@ -9,11 +79,8 @@ TEST(TranslatorTester, simulateAAAx100xlowInitxHighTerm)
   char const *home_dir;
   home_dir = getenv("HOME");
   std::string home_path(home_dir);
-  ts.loadConcentrations(home_path +
-                        "/Projects/RSim/data_2011/concentrations.csv");
-  ts.loadMRNA(home_path +
-              "/Projects/translation_results/initiation_frequency_tests/"
-              "20-09-2017/genes/AAA.txt");
+  ts.loadConcentrationsFromString(concentrationsString);
+  ts.inputMRNA(mRNA_aaa);
   ts.setInitiationRate(0.001);
   ts.setTerminationRate(1000);
   ts.setFinishedRibosomes(1000);
@@ -51,11 +118,8 @@ TEST(TranslatorTester, checkSpaceBetweenRibosomes)
   char const *home_dir;
   home_dir = getenv("HOME");
   std::string home_path(home_dir);
-  ts.loadConcentrations(home_path +
-                        "/Projects/RSim/data_2011/concentrations.csv");
-  ts.loadMRNA(home_path +
-              "/Projects/translation_results/initiation_frequency_tests/"
-              "20-09-2017/genes/AAA.txt");
+  ts.loadConcentrationsFromString(concentrationsString);
+  ts.inputMRNA(mRNA_aaa);
   ts.setInitiationRate(100);
   ts.setTerminationRate(1000);
   ts.setFinishedRibosomes(1000);
@@ -71,4 +135,42 @@ TEST(TranslatorTester, checkSpaceBetweenRibosomes)
   }
 }
 
+TEST(TranslatorTester, loadConcentrationsFromString)
+{
+  Simulations::Translation ts;
+  
+  ts.loadConcentrationsFromString(concentrationsString);
+  ts.inputMRNA(mRNA_aaa);
+  
+  
 
+  ts.setInitiationRate(0.001);
+  ts.setTerminationRate(1000);
+  ts.setFinishedRibosomes(1000);
+
+  ts.setPrepopulate(false);
+  ts.run();
+  ts.getAverageTimes();
+
+  std::vector<double> enlongation_duration;
+  std::vector<int> iteration_initiation;
+  std::tie(enlongation_duration, iteration_initiation) =
+      ts.getElongationDuration();
+  ts.getInitiationElongationTermination();
+  double total = 0;
+  for (auto dur : ts.elongations_durations)
+    total += dur;
+  double average = total / ts.elongations_durations.size();
+  ASSERT_LE(average / 100, 0.052 * 1.1);
+  ASSERT_GE(average / 100, 0.052 * 0.9);
+  std::cerr << "\naverage enlongation: " << average;
+  total = 0;
+  for (unsigned int i = 1; i < ts.codons_average_occupation_time.size() - 1;
+       i++)
+    total += ts.codons_average_occupation_time[i];
+  average = total / ts.codons_average_occupation_time.size();
+  // 0.052+-10% tolerance is acceptable
+  ASSERT_LE(average, 0.052 * 1.1);
+  ASSERT_GE(average, 0.052 * 0.9);
+  std::cerr << "\naverage codon_usage: " << average << "\n";
+}
